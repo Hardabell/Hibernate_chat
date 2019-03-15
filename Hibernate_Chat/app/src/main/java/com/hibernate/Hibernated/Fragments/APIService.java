6b793ce1c3a1e05bb6ffce614ea9ee558b0fb485
Hardabell <hardabell@gmail.com>
2019-03-15 21:07:09 +0700

    hibernate chat first

---
+++ Hibernate_Chat/app/src/main/java/com/hibernate/Hibernated/Fragments/APIService.java
@@ -0,0 +1,21 @@
+package com.hibernate.Hibernated.Fragments;
+
+import com.hibernate.Hibernated.Notifications.MyResponse;
+import com.hibernate.Hibernated.Notifications.Sender;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @Headers(
+            {
+                    "Content-Type:application/json",
+                    "Authorization:key=ADD HERE YOUR KEY FROM FIREBASE PROJECT"
+            }
+    )
+
+    @POST("fcm/send")
+    Call<MyResponse> sendNotification(@Body Sender body);
+}
---
+++ Hibernate_Chat/app/src/main/java/com/hibernate/Hibernated/Notifications/MyResponse.java
@@ -0,0 +1,6 @@
+package com.hibernate.Hibernated.Notifications;
+
+public class MyResponse {
+
+    public int success;
+}
